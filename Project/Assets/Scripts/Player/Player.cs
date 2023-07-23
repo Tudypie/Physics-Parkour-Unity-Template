4e0758fedba9a3c772bf3f7829ecd861c7a2eed6
Tudypie <33548577+Tudypie@users.noreply.github.com>
2023-07-23 18:50:29 +0300

    Unity 2022.3.5f1 project & Player Movement

---
+++ Project/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Refereces")]
+    public Transform trans;
+    public Transform modelHolder;
+    public CharacterController charController;
+
+    [Header("Gravity")]
+    public float maxGravity = 92;
+    public float timeToMaxGravity = .6f;
+    public float GravityPerSecond
+    {
+        get
+        {
+            return maxGravity / timeToMaxGravity;
+        }
+    }
+
+    private float yVelocity = 0;
+
+    [Header("Movement")]
+    public float movespeed = 42;
+    public float timeToMaxSpeed = .3f;
+    public float timeToLoseMaxSpeed = .2f;
+    public float reverseMomentumMultiplier = .6f;
+    public float midairMovementMultiplier = .4f;
+    public float bounciness = .2f;
+    private Vector3 localMovementDirection = Vector3.zero;
+    private Vector3 worldVelocity = Vector3.zero;
+    private bool grounded = false;
+
+    public float VelocityGainPerSecond
+    {
+        get
+        {
+            if(grounded)
+                return movespeed / timeToMaxSpeed;
+            else
+                return (movespeed / timeToMaxSpeed) * midairMovementMultiplier;
+        }
+    }
+
+    public float VelocityLossPerSecond
+    {
+        get
+        {
+            return movespeed / timeToLoseMaxSpeed;
+        }
+    }
+
+    [Header("Jumping")]
+    public float jumpPower = 76;
+
+    [Header("Wall Jumping")]
+    public float wallJumpPower = 40;
+    public float wallJumpAir = 56;
+    public float wallDetectionRange = 2.4f;
+    public float wallJumpCooldown = .3f;
+    public LayerMask wallDetectionLayerMask;
+    private float lastWallJumpTime;
+
+    private bool WallJumpIsOffCooldown
+    {
+        get
+        {
+            return Time.time > lastWallJumpTime + wallJumpCooldown;
+        }
+    }
+
+    private bool WallIsNearby()
+    {
+        return Physics.OverlapBox(
+            trans.position + Vector3.up * (charController.height * .5f),
+            Vector3.one * wallDetectionRange,
+            modelHolder.rotation,
+            wallDetectionLayerMask.value).Length > 0;
+    }
+
+    void Movement()
+    {
+        localMovementDirection = Vector3.zero;
+
+        //Right and left
+        if(Input.GetKey(KeyCode.D))
+            localMovementDirection.x = 1;
+        else if(Input.GetKey(KeyCode.A))
+            localMovementDirection.x = -1;
+
+        //Forward and back
+        if(Input.GetKey(KeyCode.W))
+            localMovementDirection.z = 1;
+        else if(Input.GetKey(KeyCode.S))
+            localMovementDirection.z = -1;
+
+        if(localMovementDirection != Vector3.zero)
+        {
+            Vector3 WorldMovementDirection = modelHolder.TransformDirection(localMovementDirection.normalized);
+
+            float multiplier = 1;
+            float dot = Vector3.Dot(WorldMovementDirection.normalized, worldVelocity.normalized);
+
+            if(dot < 0)
+                multiplier += -dot * reverseMomentumMultiplier;
+
+            Vector3 newVelocity = worldVelocity + WorldMovementDirection * 
+            VelocityGainPerSecond * multiplier * Time.deltaTime;
+
+            if(worldVelocity.magnitude > movespeed)
+                worldVelocity = Vector3.ClampMagnitude(newVelocity, worldVelocity.magnitude);
+            else
+                worldVelocity = Vector3.ClampMagnitude(newVelocity, movespeed);
+        }
+    }
+
+    void VelocityLoss()
+    {
+        if(grounded && (localMovementDirection == Vector3.zero || worldVelocity.magnitude > movespeed))
+        {
+            float velocityLoss = VelocityLossPerSecond * Time.deltaTime;
+
+            if(velocityLoss > worldVelocity.magnitude)
+                worldVelocity = Vector3.zero;
+            else    
+                worldVelocity -= worldVelocity.normalized * velocityLoss;
+        }
+    }
+
+    void Gravity()
+    {
+        if(!grounded && yVelocity > -maxGravity)
+            yVelocity = Mathf.Max(yVelocity - GravityPerSecond * Time.
+            deltaTime, -maxGravity);
+    }
+
+    void WallJumping()
+    {
+        if(!grounded && WallJumpIsOffCooldown)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(WallIsNearby())
+                {
+                    if(localMovementDirection != Vector3.zero)
+                    {
+                        worldVelocity = modelHolder.TransformDirection(localMovementDirection) * wallJumpPower;
+
+                        if(yVelocity <= 0)
+                            yVelocity = wallJumpAir;
+                        else
+                            yVelocity += wallJumpAir;
+
+                        lastWallJumpTime = Time.time;
+                    }
+                }
+            }
+        }
+    }
+
+    void Jumping()
+    {
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            yVelocity = jumpPower;
+            grounded = false;
+        }
+    }
+
+    void ApplyVelocity()
+    {
+        if(grounded)
+            yVelocity = -1;
+
+        Vector3 movementThisFrame = (worldVelocity + (Vector3.up * yVelocity)) * Time.deltaTime;
+        Vector3 predictedPosition = trans.position + movementThisFrame;
+
+        if(movementThisFrame.magnitude > .03f)
+            charController.Move(movementThisFrame);
+
+        //Checking ground state:
+        if(!grounded && charController.collisionFlags.HasFlag(CollisionFlags.Below))
+            grounded = true;
+        else if(grounded && !charController.collisionFlags.HasFlag(CollisionFlags.Below))
+            grounded = false;
+
+        //Bounce of walls:
+        if(!grounded && charController.collisionFlags.HasFlag(CollisionFlags.Sides))
+            worldVelocity = (trans.position - predictedPosition).normalized * (worldVelocity.magnitude *bounciness);
+        
+        //Lose Y velocity if player goes up and collides with something above
+        if(yVelocity > 0 && charController.collisionFlags.HasFlag(CollisionFlags.Above))
+            yVelocity = 0;
+    }
+
+    
+    public void AddVelocity(Vector3 amount)
+    {
+        worldVelocity += new Vector3(amount.x, 0, amount.z);
+
+        yVelocity += amount.y;
+
+        if(yVelocity > 0)
+            grounded = false;
+    }
+   
+    void Update()
+    {
+        Movement();
+        VelocityLoss();
+        Gravity();
+        WallJumping();
+        Jumping();
+        ApplyVelocity();
+    }
+}
---
+++ Project/Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [Header("References")]
+    public Transform playerBaseTrans;
+    public Transform trans;
+
+    public Transform cameraXTarget;
+    public Transform cameraYTarget;
+
+    public Transform modelHolder;
+
+    [Header("Movement and Positioning")]
+    public float rotationSpeed = 2.5f;
+    [Range(0, .99f)]
+    public float thirdPersonSmoothing = .25f;
+    public float wallMargin = .5f;
+    [Range(0, .99f)]
+    public float firstPersonSmoothing = .8f;
+    public Vector3 firstPersonLocalPosition = new Vector3(0.5f, .4f, 0);
+    public Vector3 thirdPersonLocalOrbitPosition = new Vector3(0.5f, .4f, 0);
+
+    [Header("Bounds")]
+    public float minThirdPersonDistance = 5;
+    public float maxThirdPersonDistance = 42;
+    public float thirdPersonDistance = 28;
+    public float scrollSensivity = 8;
+    public int xLookingDown = 65;
+    public int xLookingUp= 310;
+    
+    [Header("Misc")]
+    public LayerMask thirdPersonRayLayermask;
+    public KeyCode modeToggleHotkey = KeyCode.C;
+    public KeyCode mouseCursorShowHotkey = KeyCode.V;
+    public bool firstPerson = true;
+    private bool showingMouseCursor = false;
+    private Vector3 thirdPersonTargetPosition;
+    
+    private Vector3 OrbitPoint
+    {
+        get
+        {
+            return modelHolder.TransformPoint(thirdPersonLocalOrbitPosition);
+        }
+    }
+
+    private Quaternion TargetRotation
+    {
+        get
+        {
+            return Quaternion.Euler(cameraXTarget.
+            eulerAngles.x, cameraYTarget.eulerAngles.y, 0);
+        }
+    }
+    
+    private Vector3 TargetForwardDirection
+    {
+        get
+        {
+            return TargetRotation * Vector3.forward;
+        }
+    }
+    
+
+    void Start()
+    {
+        SetMouseShowing(false);  
+    }
+ 
+    void Update()
+    {
+        Hotkeys();
+    }
+
+    void LateUpdate()
+    {
+        if(!showingMouseCursor){
+            UpdateTargetRotation();
+        }
+
+        if(firstPerson){
+            FirstPerson();
+        }
+        else{
+            ThirdPerson();
+        }
+
+    }
+
+    void Hotkeys()
+    {
+        //first and third person toggle
+        if(Input.GetKeyDown(modeToggleHotkey))
+        {
+            firstPerson = !firstPerson;
+        }
+
+        //mouse mode toggle
+        if(Input.GetKeyDown(mouseCursorShowHotkey))
+        {
+            SetMouseShowing(true);
+        }
+        if(Input.GetKeyUp(mouseCursorShowHotkey))
+        {
+            SetMouseShowing(false);
+        }
+
+        if(!firstPerson)
+        {
+            float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
+
+            thirdPersonDistance = Mathf.Clamp(thirdPersonDistance - scrollDelta * scrollSensivity, minThirdPersonDistance, maxThirdPersonDistance);
+        }
+    }
+
+    void SetMouseShowing(bool value)
+    {
+        Cursor.visible = value;
+        showingMouseCursor = value;
+
+        if(value)
+            Cursor.lockState = CursorLockMode.None;
+        else   
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void UpdateTargetRotation()
+    {
+        float xRotation = (Input.GetAxis("Mouse Y") * -rotationSpeed);
+        float yRotation = (Input.GetAxis("Mouse X") * rotationSpeed);
+
+        cameraXTarget.Rotate(xRotation, 0, 0);
+        cameraYTarget.Rotate(0, yRotation, 0);
+
+        if(cameraXTarget.localEulerAngles.x >= 180)
+        {
+            if(cameraXTarget.localEulerAngles.x < xLookingUp)
+                cameraXTarget.localEulerAngles = new Vector3(xLookingUp, 0, 0);
+        }
+        else
+        {
+            if(cameraXTarget.localEulerAngles.x > xLookingDown)
+                cameraXTarget.localEulerAngles = new Vector3(xLookingDown, 0, 0);
+        }
+    }
+
+    void FirstPerson()
+    {
+        Vector3 targetWorldPosition = modelHolder.TransformPoint(firstPersonLocalPosition);
+
+        if(trans.position != targetWorldPosition)
+        {
+            trans.position = Vector3.Lerp(trans.position, targetWorldPosition, .2f);
+        }
+
+        Quaternion targetRotation = Quaternion.Slerp(trans.rotation, TargetRotation, 1.0f - firstPersonSmoothing);
+
+        trans.eulerAngles = new Vector3(targetRotation.eulerAngles.x, targetRotation.eulerAngles.y, 0);
+
+        modelHolder.forward = new Vector3(trans.forward.x, 0, trans.forward.z);
+    }
+
+    void ThirdPerson()
+    {
+        Ray ray = new Ray(OrbitPoint, -TargetForwardDirection);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, thirdPersonDistance + wallMargin, thirdPersonRayLayermask.value))
+        {
+            thirdPersonTargetPosition = hit.point;
+            thirdPersonTargetPosition += (TargetForwardDirection * wallMargin);
+        }
+        else
+        {
+            thirdPersonTargetPosition = OrbitPoint - (TargetForwardDirection * thirdPersonDistance);
+        }
+
+        trans.position = Vector3.Lerp(trans.position, thirdPersonTargetPosition, 1.0f - thirdPersonSmoothing);
+        
+        trans.forward = (OrbitPoint - trans.position).normalized;
+
+        modelHolder.forward = new Vector3(trans.forward.x, 0, trans.forward.z);
+    }
+}
